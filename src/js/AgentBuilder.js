21741d5d593583d049270f09db29d9418f31e8f7
joski2490 <68449917+joski2490@users.noreply.github.com>
2023-01-15 15:01:16 -0600

    Update AgentBuilder.js

--- src/js/AgentBuilder.js
+++ src/js/AgentBuilder.js
@@ -76,9 +76,9 @@ let Agent = new AgentBuilder('CREATURE');
  */
 let Predator = new AgentBuilder('PREDATOR')
   .setRadius(10)
-  .setMaxSpeed(2)
-  .setMaxForce(0.05)
-  .setHealthDecrease(0.002)
+  .setMaxSpeed(4)//2
+  .setMaxForce(0.5)//0.05
+  .setHealthDecrease(0.2)//0.002
   .setColor([255, 0, 0])
   .setFoodMultiplier([0.5, -0.5]);
 
